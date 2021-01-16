0e93a522dddcc7c4b92346798621c212c5363697
IT <Yaln retim 2@LAPTOP-2K2NHRHM.AKCAN.local>
2021-01-16 23:56:18 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array = dizi 
+
+            string[] kurslar = new string[] {"Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlangıç için temel kurs", "JAVA", "C#", "Python" };
+           
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("<<<for bitti>>>");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Düştü");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Arttı");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Giriş Yapıldı");
+                Console.WriteLine(ogrenciSayisi);
+            }
+            else
+            {
+                Console.WriteLine("Lütfen Giriş Yapınız");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
